a9912659c21c36bd9e0bc15866ddbdd78d08f2db
Rishav Bhagat <rishavb123@gmail.com>
2020-05-30 02:10:16 -0400

    created progress bar class

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
 NOISE_DIM = 100
 IMAGE_DIM = (100, 72)
+
+PROGRESS_BAR = {
+    'width': 100,
+    'positive': '+',
+    'negative': '-'
+}
---
+++ progress_bar.py
@@ -0,0 +1,25 @@
+import time
+import datetime
+
+from config import PROGRESS_BAR
+
+class ProgressBar:
+
+    def __init__(self, length):
+        self.length = length
+        self.i = 0
+        self.show()
+        self.init_time = time.time()
+        self.done = False
+
+    def show(self):
+        progress = int(self.i / self.length * PROGRESS_BAR['width'])
+        print('[' + PROGRESS_BAR['positive'] * progress + PROGRESS_BAR['negative'] * (PROGRESS_BAR['width'] - progress) + '] ' + str(self.i) + ' / ' + str(self.length) + ' ' + str(datetime.timedelta(seconds=int(time.time() - self.init_time))), end='\r')
+
+    def increment(self):
+        if self.done: return
+        self.i += 1
+        self.show()
+        if self.i >= self.length:
+            self.done = True
+            print()
