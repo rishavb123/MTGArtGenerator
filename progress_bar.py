d3aefc49df97b50e8eb5093fca89364968acfd65
Rishav Bhagat <rishavb123@gmail.com>
2020-05-30 02:29:49 -0400

    minor changes

--- progress_bar.py
+++ progress_bar.py
@@ -14,7 +14,9 @@ class ProgressBar:
 
     def show(self):
         progress = int(self.i / self.length * PROGRESS_BAR['width'])
-        print('[' + PROGRESS_BAR['positive'] * progress + PROGRESS_BAR['negative'] * (PROGRESS_BAR['width'] - progress) + '] ' + str(self.i) + ' / ' + str(self.length) + ' ' + str(datetime.timedelta(seconds=int(time.time() - self.init_time))), end='\r')
+        t = int(time.time() - self.init_time)
+        r = int(t * (self.length / max(1, self.i) - 1))
+        print('[' + PROGRESS_BAR['positive'] * progress + PROGRESS_BAR['negative'] * (PROGRESS_BAR['width'] - progress) + ']', str(self.i), '/', str(self.length), 'Time Elapsed:', str(datetime.timedelta(seconds=t)), 'Estimated Time Remaining:', str(datetime.timedelta(seconds=r)), end='\r')
 
     def increment(self):
         if self.done: return
@@ -22,4 +24,4 @@ class ProgressBar:
         self.show()
         if self.i >= self.length:
             self.done = True
-            print("Finished in", str(datetime.timedelta(seconds=int(time.time() - self.init_time))))
+            print("\nFinished in", str(datetime.timedelta(seconds=int(time.time() - self.init_time))))
